1b159f35611b748c227d8ed97211febd6a80bab9
ACANX <acanx@qq.com>
2025-08-19 11:51:23 +0800

    Auto commit
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ Python/MetadataWorkflow.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd `dirname $0`
+PATH=$PATH:/usr/local/bin
+# 休眠随机时间0-99s
+#sleep $[$RANDOM%100]
+python3 MetadataWorkflow.py
+echo "MetadataWorkflow.py script finished！"
+python3 CNFundList.py
+echo "CNFundList.py script finished！"
