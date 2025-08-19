02163fe9478fd14296b339cde0703360505b7da2
ACANX <acanx@qq.com>
2025-08-19 10:47:08 +0800

    Update CNFundList.py

--- Python/CNFundList.py
+++ Python/CNFundList.py
@@ -96,7 +96,7 @@ def generate_markdown_table(funds_data):
         # 创建带链接的基金经理
         manager_link = generateManagerLinkText(fund['manager'])
         # 带链接的最新净值
-        nav_link = f"[{fund['nav']}({fund['nage_change_rate']}%)](https://fund.eastmoney.com/{fund['code']}.html)"
+        nav_link = f"[{fund['nav']}({fund['nav_change_rate']}%)](https://fund.eastmoney.com/{fund['code']}.html)"
         # 添加表格行
         table += f"| {code_link} | {name_link} |{company_link} | {manager_link} | {fund['fund_type']} | {fund['issue_date']} | {fund['assets_size']} | {fund['assets_size_date']} | {nav_link} |\n"
     return table
